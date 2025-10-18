7e581c408cc3d023a7f9b778f2c4726d084de2e3
Bowen <tanbw1996@gmail.com>
2025-10-18 16:46:15 +0200

    Create datasets.py

---
+++ training/datasets.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_diabetes
+import pandas as pd
+
+
+def load_diabetes_df():
+    Xy = load_diabetes(as_frame=True)
+    df = Xy.frame
+    # Ensure feature names are human-friendly to match API spec
+    # scikit-learn uses these keys already: age, sex, bmi, bp, s1..s6
+    return df.drop(columns=["target"]), df["target"]
